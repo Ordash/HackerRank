380c506e47ae0377e2c766e0d054b2ae8187a7b2
Ordash <farkas.bako@gmail.com>
2019-02-24 13:42:42 +0100

    added sock marchant

---
+++ InterviewPreparationKit/src/SockMerchant.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class SockMerchant {
+
+    public static void main(String[] args) {
+
+        int n = 8;
+        int[] ar = {1,2,5,2,6,7,7,1};
+        //int[] ar = {1, 1, 1, 1, 1, 1, 1, 1};
+
+        System.out.println(sockMerchant(n, ar));
+
+    }
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        HashMap<Integer, Integer> sockMap = new HashMap<>();
+        int sockPairs = 0;
+        for (int i = 0; i < n; i++) {
+            Integer c = sockMap.get(ar[i]);
+            if (sockMap.get(ar[i]) == null) {
+                sockMap.put(ar[i], 1);
+            } else {
+                sockMap.put(ar[i], ++c);
+            }
+        }
+        for (Object o : sockMap.entrySet()) {
+            Map.Entry pair = (Map.Entry) o;
+            Integer totalCountOfSock = (Integer) pair.getValue();
+            sockPairs += totalCountOfSock / 2;
+        }
+        return sockPairs;
+    }
+}
